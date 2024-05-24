9de3b369d2011236a1f8935bffeec078dc48b77a
jayrkyd <knowlesjordy@gmail.com>
2024-05-23 23:05:27 -0400

    changes

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,6 @@
 import { clerkMiddleware, authMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/events/:id',
-    '/api/webhook/clerk',
-
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-
-    '/api/uploadthing'
-  ]
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\..*|_next).*)", "/", "/(api|trpc)(.*)"],
