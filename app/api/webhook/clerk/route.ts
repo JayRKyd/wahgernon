29d54b685ed3f0c9fb89f3ba47166bba3db876f0
jayrkyd <knowlesjordy@gmail.com>
2024-05-23 22:28:52 -0400

    fixing issues

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -61,8 +61,8 @@ export async function POST(req: Request) {
         clerkId: id,
         email: email_addresses[0].email_address,
         username: username!,
-        firstName: first_name!,
-        lastName: last_name! ,
+        firstName: first_name || '',
+        lastName: last_name || '' ,
         photo: image_url,
       }
 
@@ -84,8 +84,8 @@ export async function POST(req: Request) {
 
     const user = {
       firstName: first_name!,
-      lastName: last_name!,
-      username: username!,
+      lastName: last_name || '',
+      username: username || '',
       photo: image_url,
     }
 
